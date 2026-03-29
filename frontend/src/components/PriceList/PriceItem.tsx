dd0a17ea47c089befe84066238b4b0e964360596
Ruslan Kulish <ruslan.s.kulish@gmail.com>
2026-03-29 18:22:24 +0300

    добавлена пагинация, обновлены фильтры

--- frontend/src/components/PriceList/PriceItem.tsx
+++ frontend/src/components/PriceList/PriceItem.tsx
@@ -6,24 +6,60 @@ import './PriceItem.scss';
 
 interface IPriceItemProps {
   item: TPriceItemExtendedResponse;
-  // onClick: (item: TPriceItemExtendedResponse) => void;
 }
 
-const cnStyles = block('price-item-container');
+const cnStyles = block('price-item');
 
-export const PriceItem: FC<IPriceItemProps> = ({ item, onClick }) => {
+export const PriceItem: FC<IPriceItemProps> = ({ item }) => {
   return (
-    <Link 
+    <Link
       to={{ pathname: `/price/${item.id}` }}
       state={{ backgroundLocation: true }}
       className={cnStyles()}
-    
     >
-      {/* <article className={cnStyles('priceitem')} onClick={() => onClick(item)}> */}
-      <article className={cnStyles('priceitem')}>
-        <div className={cnStyles('name')}>{item.name}</div>
-        <div className={cnStyles('size')}>{item.size}</div>
-        <div className={cnStyles('price')}>{item.pricePer1tn.toLocaleString('ru-RU')} ₽</div>
+      <article className={cnStyles('card')}>
+        {/* Левая часть: название + мета-бейджи */}
+        <div className={cnStyles('left')}>
+          <span className={cnStyles('name')}>{item.nameExt}</span>
+          <div className={cnStyles('badges')}>
+            {item.other && (
+              <span className={cnStyles('badge', 'other')}>{item.other}</span>
+            )}
+            {item.surface && (
+              <span className={cnStyles('badge', 'surface')}>{item.surface}</span>
+            )}
+          </div>
+        </div>
+
+        {/* Средняя колонка: вес 1 шт */}
+        <div className={cnStyles('col-weight')}>
+          {item.unitWeight > 0 && (
+            <>
+              <span className={cnStyles('col-weight__value')}>{item.unitWeight}</span>
+              <span className={cnStyles('col-weight__unit')}>кг/шт</span>
+            </>
+          )}
+        </div>
+
+        {/* Правая часть: диаметр + цены */}
+        <div className={cnStyles('right')}>
+          <span className={cnStyles('size')}>{item.size}</span>
+          <div className={cnStyles('prices')}>
+            <span className={cnStyles('price-main')}>
+              {item.pricePer1tn.toLocaleString('ru-RU')} ₽/т
+            </span>
+            {item.pricePer5tn > 0 && item.pricePer5tn !== item.pricePer1tn && (
+              <span className={cnStyles('price-sub')}>
+                от 5т: {item.pricePer5tn.toLocaleString('ru-RU')} ₽
+              </span>
+            )}
+            {item.pricePer15tn > 0 && item.pricePer15tn !== item.pricePer5tn && (
+              <span className={cnStyles('price-sub')}>
+                от 15т: {item.pricePer15tn.toLocaleString('ru-RU')} ₽
+              </span>
+            )}
+          </div>
+        </div>
       </article>
     </Link>
   );
